29de80c2d435b56adc49ceff815e8faed4d0f715
bolaalcatras <jhonalexander2016.com@gmail.com>
2026-01-16 15:34:22 -0500

    feat: create Etiqueta entity for tag management.

---
+++ src/modules/tags/entities/etiqueta.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('tm_etiqueta')
+export class Etiqueta {
+    @PrimaryGeneratedColumn({ name: 'eti_id' })
+    id: number;
+
+    @Column({ name: 'usu_id', type: 'int', comment: 'ID del usuario propietario de la etiqueta' })
+    usuarioId: number;
+
+    @Column({ name: 'eti_nom', type: 'varchar', length: 150 })
+    nombre: string;
+
+    @Column({ name: 'eti_color', type: 'varchar', length: 50 })
+    color: string;
+
+    @Column({ name: 'fech_crea', type: 'datetime', nullable: true })
+    fechaCreacion: Date | null;
+
+    @Column({ name: 'est', type: 'int', default: 1 })
+    estado: number;
+
+    // TODO: Agregar relaciones
+    // @ManyToOne(() => User)
+    // @JoinColumn({ name: 'usu_id' })
+    // usuario: User;
+
+    // @OneToMany(() => TicketEtiqueta, (te) => te.etiqueta)
+    // ticketEtiquetas: TicketEtiqueta[];
+}
