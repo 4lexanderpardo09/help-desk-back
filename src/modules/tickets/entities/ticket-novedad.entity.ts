2eb41f9c256a4d84c8b1ca1312ff231dcd01d76f
bolaalcatras <jhonalexander2016.com@gmail.com>
2026-01-16 15:15:20 -0500

    feat: create TicketNovedad TypeORM entity with all its properties.

---
+++ src/modules/tickets/entities/ticket-novedad.entity.ts
@@ -0,0 +1,48 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('th_ticket_novedad')
+export class TicketNovedad {
+    @PrimaryGeneratedColumn({ name: 'novedad_id' })
+    id: number;
+
+    @Column({ name: 'tick_id', type: 'int' })
+    ticketId: number;
+
+    @Column({ name: 'paso_id_pausado', type: 'int' })
+    pasoPausadoId: number;
+
+    @Column({ name: 'usu_asig_novedad', type: 'int' })
+    usuarioAsignadoNovedadId: number;
+
+    @Column({ name: 'usu_crea_novedad', type: 'int' })
+    usuarioCreadorNovedadId: number;
+
+    @Column({ name: 'descripcion_novedad', type: 'text' })
+    descripcion: string;
+
+    @Column({ name: 'fecha_inicio', type: 'datetime' })
+    fechaInicio: Date;
+
+    @Column({ name: 'fecha_fin', type: 'datetime', nullable: true })
+    fechaFin: Date | null;
+
+    @Column({ name: 'estado_novedad', type: 'enum', enum: ['Abierta', 'Resuelta'], default: 'Abierta' })
+    estado: 'Abierta' | 'Resuelta';
+
+    // TODO: Agregar relaciones
+    // @ManyToOne(() => Ticket)
+    // @JoinColumn({ name: 'tick_id' })
+    // ticket: Ticket;
+
+    // @ManyToOne(() => PasoFlujo)
+    // @JoinColumn({ name: 'paso_id_pausado' })
+    // pasoPausado: PasoFlujo;
+
+    // @ManyToOne(() => User)
+    // @JoinColumn({ name: 'usu_asig_novedad' })
+    // usuarioAsignado: User;
+
+    // @ManyToOne(() => User)
+    // @JoinColumn({ name: 'usu_crea_novedad' })
+    // usuarioCreador: User;
+}
