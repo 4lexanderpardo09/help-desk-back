d2662c9100743805501e295f0a5c30e5fdaa7138
bolaalcatras <jhonalexander2016.com@gmail.com>
2026-01-16 15:00:47 -0500

    feat: create DocumentoCierre TypeORM entity.

---
+++ src/modules/tickets/entities/documento-cierre.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('td_documento_cierre')
+export class DocumentoCierre {
+    @PrimaryGeneratedColumn({ name: 'doc_cierre_id' })
+    id: number;
+
+    @Column({ name: 'tick_id', type: 'int' })
+    ticketId: number;
+
+    @Column({ name: 'doc_nom', type: 'varchar', length: 255 })
+    nombre: string;
+
+    @Column({ name: 'fech_crea', type: 'datetime' })
+    fechaCreacion: Date;
+
+    @Column({ name: 'est', type: 'int', default: 1 })
+    estado: number;
+
+    // TODO: Agregar relaciones ManyToOne
+    // @ManyToOne(() => Ticket)
+    // @JoinColumn({ name: 'tick_id' })
+    // ticket: Ticket;
+}
